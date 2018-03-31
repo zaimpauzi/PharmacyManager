86b2accb08fdb93d714efcfcf4f27de8a00c588f
Muhammad Zaim <zaimpauzi@outlook.com>
2018-03-31 18:41:59 +0800

    Add medicine object

---
+++ PharmacyManager/Models/MedicineObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManager.Models
+{
+    public class MedicineObject
+    {
+
+        public Count Count  { get; set; }
+    
+    }
+
+    public class Count
+    {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public string MinQuantity { get; set; }
+        public string MaxQuantity { get; set; }
+    }
+}
--- PharmacyManager/ViewModels/PharmacyManagerViewModel.cs
+++ PharmacyManager/ViewModels/PharmacyManagerViewModel.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using Excel = Microsoft.Office.Interop.Excel;
-using System.IO;
 using PharmacyManager.Commands;
-using PharmacyManager.Models;
 using System.Windows.Input;
 
 namespace PharmacyManager.ViewModels
@@ -16,12 +12,6 @@ namespace PharmacyManager.ViewModels
     class PharmacyManagerViewModel : ViewModelBase
     {
 
-        private string str;
-        private string[] med = new string[13];
-        private int rCnt;
-        private int cCnt;
-        private int rw = 0;
-        private int cl = 0;
         private string icInput;
 
         private DelegateCommand searchCommand;
@@ -58,7 +48,9 @@ namespace PharmacyManager.ViewModels
 
         private void isSearch()
         {
-            var Patient = GetPatientObject(icInput);
+
+            var GetObject = new GetObjectsViewModel();
+            var Patient = GetObject.GetPatientObject(icInput);
             if (Patient != null)
             {
                 PrintName = Patient.Name;
@@ -223,69 +215,9 @@ namespace PharmacyManager.ViewModels
                 PrintMed8 = string.Empty;
                 PrintMed9 = string.Empty;
                 PrintMed10 = string.Empty;
-
-
-
         }
 
-        public PatientObject GetPatientObject(string ic)
-        {
-            string currentDir = Environment.CurrentDirectory;
-            string filePath = "Database.xlsx";
-            string fullPath = Path.Combine(currentDir, filePath);
-            var patientObject = new PatientObject();
-            var xlApp = new Excel.Application();
-            var xlWorkBook = xlApp.Workbooks.Open(fullPath, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-            var xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-            var range = xlWorkSheet.UsedRange;
-            rw = range.Rows.Count;
-            cl = range.Columns.Count;
-            bool isExist = false;
-
-
-            for (rCnt = 1; rCnt <= rw; rCnt++)
-            {
-               double catchIC = (double)(range.Cells[rCnt, 1] as Excel.Range).Value2;
-                str = catchIC.ToString();
-                if (str == ic)
-                {
-                    //MessageBox.Show(str);
-                    for (cCnt = 2; cCnt <= cl; cCnt++)
-                    {
-                        med[cCnt] = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
-                        //MessageBox.Show(med[cCnt]);
-                    }
-                    isExist = true;
-                }
-            }
-
-            patientObject.Name = med[2];
-            patientObject.Medicine1 = med[3];
-            patientObject.Medicine2 = med[4];
-            patientObject.Medicine3 = med[5];
-            patientObject.Medicine4 = med[6];
-            patientObject.Medicine5 = med[7];
-            patientObject.Medicine6 = med[8];
-            patientObject.Medicine7 = med[9];
-            patientObject.Medicine8 = med[10];
-            patientObject.Medicine9 = med[11];
-            patientObject.Medicine10 = med[12];
-
-            xlWorkBook.Close(true, null, null);
-            xlApp.Quit();
-
-            Marshal.ReleaseComObject(xlWorkSheet);
-            Marshal.ReleaseComObject(xlWorkBook);
-            Marshal.ReleaseComObject(xlApp);
-            if (isExist)
-            {
-                return patientObject;
-            }
-            else
-            {
-                return null;
-            }
-        }
+        
 
     }
 }
